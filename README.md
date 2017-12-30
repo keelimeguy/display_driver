d8668d9e7f5ddfb45061c344fe7bf72c5dce8ed1
Keelin Wheeler <keelinwheeler@gmail.com>
2017-12-30 18:19:32 -0500

    Implementing simplified graphics library based on Adafruit GFX

---
+++ README.md
@@ -0,0 +1,10 @@
+## Display Driver - an API for driving basic graphics on displays
+
+### Command Console Implementation:
+run `make` then `./test.exe`
+
+### Adafruit GFX Library source:
+- [GFX Library](https://github.com/adafruit/Adafruit-GFX-Library)
+
+### Touch Controller (unimplemented):
+- [Touch Controller Information](http://www.lysator.liu.se/~kjell-e/embedded/doc8091.pdf)
---
+++ test_display_driver_main.c
@@ -0,0 +1,20 @@
+#include "display_driver.h"
+#include "GFXfont/Fonts/FreeMono9pt7b.h"
+
+int main() {
+    disp_init();
+    disp_set_font(&FreeMono9pt7b);
+
+    disp_start_write();
+    disp_set_pos(0,0);
+    disp_write_str("\\Hello, world!\n:)");
+    disp_end_write();
+    uint8_t x1;
+    uint8_t y1;
+    uint8_t w;
+    uint8_t h;
+    disp_get_text_bounds("\\Hello, world!", 0, 0, &x1, &y1, &w, &h);
+    printf("%d, %d, %d, %d\n", x1, y1, w, h);
+
+    return 0;
+}
